cb668f0775bff5c97b9addfc6a6ed1d972a665f6
emeseJW <wagnermese@gmail.com>
2023-07-25 14:12:14 +0200

    add new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyFirstComponentInMYLIFEComponent } from './my-first-component-in-mylife/my-first-component-in-mylife.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponentInMYLIFEComponent
   ],
   imports: [
     BrowserModule,
